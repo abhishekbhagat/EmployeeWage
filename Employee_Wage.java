dfa0253b69de3103e077a6c47e8db9f669fa8359
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 07:50:56 +0530

    [abhishek] check employee is present or not

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.problem;
-public class Employee_Wage
-{
+public class Employee_Wage{
 	public static void main(String[] args){
-            System.out.println("Welcome Employee Wage Computation");	
+             //constants
+             final int IS_PRESENT=0;
+             //computation 
+             double empCheck= Math.floor(Math.random()*10)%2;
+	     if(empCheck==IS_PRESENT)
+	        System.out.println("Employee is Present ");
+             else
+                System.out.println("Employee is Absent "); 	
 	}
 
 }
