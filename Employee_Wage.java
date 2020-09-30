35d80c91060ebddd75aee3976fe89074c273c17b
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 07:43:37 +0530

    [abhishek] welcome message

---
+++ Employee_Wage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.problem;
+public class Employee_Wage
+{
+	public static void main(String[] args){
+            System.out.println("Welcome Employee Wage Computation");	
+	}
+
+}
