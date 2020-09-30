8de234c216001f9c6e6ed7ceb20065a628ee6306
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 10:03:56 +0530

    [abhishek] calculate till condition of working hour or max days reache certain value

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -3,29 +3,35 @@ public class Employee_Wage{
 	public static final int IS_FULL_TIME = 0;
         public static final int IS_PART_TIME=1;
         public static final int EMP_WAGE_PER_HOUR=20;
-        public static final int NUM_OF_WORKING_DAYS=25;
+        public static final int NUM_OF_WORKING_DAYS=25; 
+        public static final int MAX_HRS_IN_MONTH=100;
+       
 	public static void main(String[] args){
         //variables
         int empHrs=0;
 	int empWage=0;
         int totalWage=0;
+        int totalWorkingDays=0;
           //Computation
-	int empCheck= (int)Math.floor(Math.random()*10)%2;
-        for(int day=1;day<=NUM_OF_WORKING_DAYS;day++){
+        while(true){
+        totalWorkingDays++;
+        int empCheck= (int)Math.floor(Math.random()*10)%2;
 	switch(empCheck){
-		  case IS_FULL_TIME:
-		       empHrs=8;
-		       break;
-		       
-		  case IS_PART_TIME:
-		      empHrs=4;
-		      break;
-		  default:
-			empHrs=0;
+	    case IS_FULL_TIME:
+               empHrs=8;
+	       break;       
+	    case IS_PART_TIME:
+	       empHrs=4;
+	       break;
+   	    default: 
+	       empHrs=0;
 	}
 	empWage=empHrs*EMP_WAGE_PER_HOUR;
         totalWage+=empWage;
+        totalWorkingHour+=empHrs;
 	System.out.println("Employee Wage = "+EMP_WAGE_PER_HOUR);
+        if(totalWorkingDays==NUM_OF_WORKING_DAYS ||totalWorkingHour>=MAX_HRS_IN_MONTH)
+			   break;
         }
         System.out.println("Total Employee Wage = "+Totalwage);
     }
