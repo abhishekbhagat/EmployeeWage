6374cd839bb82bf4b73be6ead4e8dd636fde1cab
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 09:35:19 +0530

    wages for a Month

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,15 +1,17 @@
 package com.bridgelabz.problem;
 public class Employee_Wage{
+	public static final int IS_FULL_TIME = 0;
+        public static final int IS_PART_TIME=1;
+        public static final int EMP_WAGE_PER_HOUR=20;
+        public static final int NUM_OF_WORKING_DAYS=25;
 	public static void main(String[] args){
-        //constants
-	final int IS_FULL_TIME = 0;
-        final int IS_PART_TIME=1;
-        int EMP_WAGE_PER_HOUR=20;
         //variables
         int empHrs=0;
 	int empWage=0;
+        int totalWage=0;
           //Computation
 	int empCheck= (int)Math.floor(Math.random()*10)%2;
+        for(int day=1;day<=NUM_OF_WORKING_DAYS;day++){
 	switch(empCheck){
 		  case IS_FULL_TIME:
 		       empHrs=8;
@@ -20,8 +22,11 @@ public class Employee_Wage{
 		      break;
 		  default:
 			empHrs=0;
-		}
-	empWage=empHrs*EMP_WAGE_PER_HOUR;
-	System.out.println("Employee Wage = "+EMP_WAGE_PER_HOUR);	
 	}
+	empWage=empHrs*EMP_WAGE_PER_HOUR;
+        totalWage+=empWage;
+	System.out.println("Employee Wage = "+EMP_WAGE_PER_HOUR);
+        }
+        System.out.println("Total Employee Wage = "+Totalwage);
+    }
 }
