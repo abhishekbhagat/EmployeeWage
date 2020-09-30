073c6e345e194a16e86c6aae16d2efda4d5d5358
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 09:04:36 +0530

    [abhishek] use switch case

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -9,11 +9,18 @@ public class Employee_Wage{
         int empHrs=0;
 	int empWage=0;
           //Computation
-	double empCheck= Math.floor(Math.random()*10)%2;
-	if(empCheck==IS_FULL_TIME)
-		empHrs=8;
-        else
-                empHrs=4;
+	int empCheck= (int)Math.floor(Math.random()*10)%2;
+	switch(empCheck){
+		  case IS_FULL_TIME:
+		       empHrs=8;
+		       break;
+		       
+		  case IS_PART_TIME:
+		      empHrs=4;
+		      break;
+		  default:
+			empHrs=0;
+		}
 	empWage=empHrs*EMP_WAGE_PER_HOUR;
 	System.out.println("Employee Wage = "+EMP_WAGE_PER_HOUR);	
 	}
