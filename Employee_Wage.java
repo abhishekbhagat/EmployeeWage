b4cce8c1ecb2127369014be4347063d42fe2f1f6
abhishekbhagat <40999861+abhishekbhagat@users.noreply.github.com>
2020-10-01 08:39:01 +0530

    Update Employee_Wage.java

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -10,12 +10,10 @@ public class Employee_Wage implements IComputeEmpWage{
 	public static final int IS_PART_TIME=1;
 	private  LinkedList<CompanyEmpWage> companyEmpWageList;
 	private Map<String,CompanyEmpWage> companyToEmpWageMap;
-	public Employee_Wage()
-	{
+	public Employee_Wage(){
 		companyEmpWageList=new LinkedList<>();
 		companyToEmpWageMap=new HashMap<>();
 	}
-	
 	public void computeEmpWage(){
 		for(int i=0;i<companyEmpWageList.size();i++){
 			CompanyEmpWage companyEmpWage= companyEmpWageList.get(i);
@@ -23,7 +21,7 @@ public class Employee_Wage implements IComputeEmpWage{
 			System.out.println(companyEmpWage);
 		}
 	}
-	public int computeEmpWage(CompanyEmpWage companyEmpWage) {
+	public int computeEmpWage(CompanyEmpWage companyEmpWage){
 		//variables
 		int totalWage=0;
 		int totalWorkingHour=0;
@@ -41,15 +39,15 @@ public class Employee_Wage implements IComputeEmpWage{
 			         break;
 			     default:
 			    	 empHrs=0;
-			   }
+			 }
 			 totalWorkingHour=empHrs+totalWorkingHour;
 			 totalWorkingDays++;
 			 empWage=empHrs*companyEmpWage.getEmpRatePerHour();
 			 totalWage=empWage+totalWage;
 			 System.out.println("Employee Wage on "+totalWorkingDays+" Day is: "+empWage);
-			 if(totalWorkingDays==companyEmpWage.numOfWorkingDays||totalWorkingHour>=companyEmpWage.maxHoursPerMonth)
+			 if(totalWorkingDays==companyEmpWage.numOfWorkingDays()||totalWorkingHour>=companyEmpWage.getmaxHoursPerMonth())
 				   break;
-	      }
+	       }
           companyEmpWage.setTotalEmpWage(totalWage);
 	  System.out.println("Total Employee Wage = "+companyEmpWage.totalEmpWage);	
 	  return companyEmpWage.totalEmpWage;
